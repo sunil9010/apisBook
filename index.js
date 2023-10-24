e8a78f01bf74ca60b38ca18b7429302faceb0374
root <tsn9010@gmail.com>
2023-10-24 07:41:29 +0000

    solution

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const uuid = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const cors = require("cors");
@@ -44,83 +44,49 @@ app.get("/books/", async (request, response) => {
   response.send(booksArray);
 });
 
-//Get Book API
-app.get("/books/:bookId/", (request, response) => {});
-
-app.post("/api/seats", async (req, res) => {
-  const newSeats = req.body;
-  const { seat_id, seat_number, is_selected, is_occupied, category } = newSeats;
-
-  console.log(seat_id);
-
-  //   Create a SQL query to insert multiple seats in a single transaction
-  const sql = `INSERT INTO seats (seat_id,seat_number,is_selected,is_occupied,category)
-      VALUES (
-         ${seat_id},
-         '${seat_number}',
-         ${is_selected},
-         ${is_occupied},
-         '${category}'
-      );`;
-
-  // Extract an array of values for the new seats
-
-  let data = await db.run(sql);
-  res.send("Added Successfully");
-
-  // Execute the query with multiple value sets
-});
-
-// app.get("/api/seats", async (req, res) => {
-//   // Extract query parameters
-//   const { category, status } = req.query;
-
-//   // Build the SQL query dynamically based on the query parameters
-//   let sql = "SELECT * FROM seats WHERE 1";
-
-//   if (category) {
-//     sql += ` AND category = ${db.escape(category)}`;
-//   }
-
-//   if (status) {
-//     sql += ` AND status = ${db.escape(status)}`;
-//   }
-
-//   // Execute the SQL query with error handling
-//   await db.all(sql, (err, results) => {
-//     if (err) {
-//       console.error("Database error:", err.message);
-
-//       // Return a more detailed error response
-//       res
-//         .status(500)
-//         .json({ error: "Internal server error", details: err.message });
-//     } else {
-//       res.json(results);
-//     }
-//   });
-// });
-
 app.get("/api/seats", async (req, res) => {
   // Extract query parameters
-  const { category = "premium", status } = req.query;
+  const { category = "", status } = req.query;
 
   // Build the SQL query dynamically based on the query parameters
   let sql = `SELECT * FROM seats WHERE category like '%${category}%'`;
   let data = await db.all(sql);
   res.send(data);
+});
 
-  //   const params = []; // An array to store parameter values
+app.post("/api/seats", async (req, res) => {
+  const newSeats = req.body;
 
-  //   if (category) {
-  //     sql += " AND category = ?";
-  //     params.push(category);
-  //   }
+  // Create a SQL query with placeholders
+  const sql = `
+    INSERT INTO seats (seat_id, seat_number, is_selected, is_occupied, category)
+    VALUES (?, ?, ?, ?, ?);
+  `;
 
-  //   if (status) {
-  //     sql += " AND status = ?";
-  //     params.push(status);
-  //   }
+  try {
+    await db.run("BEGIN"); // Start a transaction
 
-  // Execute the SQL query with prepared statement and error handling
+    for (const seat of newSeats) {
+      const { seat_number, is_selected, is_occupied, category } = seat;
+      const seat_id = uuidv4();
+      await db.run(
+        sql,
+        seat_id,
+        seat_number,
+        is_selected,
+        is_occupied,
+        category
+      );
+    }
+
+    await db.run("COMMIT"); // Commit the transaction
+
+    res.send("Added Successfully");
+  } catch (err) {
+    console.error("Database error:", err.message);
+    await db.run("ROLLBACK"); // Rollback the transaction in case of an error
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: err.message });
+  }
 });
